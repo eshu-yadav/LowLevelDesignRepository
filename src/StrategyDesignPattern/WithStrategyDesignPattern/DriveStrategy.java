5c634ba4df71600d86c523fd4c9602b238f3cb66
eshu-yadav <eshuyadav123@gmail.com>
2024-05-26 17:46:20 +0530

    Completed Strategy Design Pattern with Diagram

--- src/StrategyDesignPattern/WithStrategyDesignPattern/DriveStrategy.java
+++ src/StrategyDesignPattern/WithStrategyDesignPattern/DriveStrategy.java
@@ -1,10 +1,4 @@
 package StrategyDesignPattern.WithStrategyDesignPattern;
 public interface DriveStrategy {
     public void drive();
-
-    class GoodsVehicle extends Vehicle {
-        GoodsVehicle(){
-            super(new NormalDriveStrategy());
-        }
-    }
 }
