fb8b4d050c64597a97b5053e66bf021f81346611
eshu-yadav <eshuyadav123@gmail.com>
2024-06-23 09:26:02 +0530

    Completed ChainOfResponsiblity Design Pattern with Diagram

---
+++ src/ChainOfResponsibilityDesignPattern/DebugLogProcessor.java
@@ -0,0 +1,14 @@
+package ChainOfResponsibilityDesignPattern;
+
+public class DebugLogProcessor extends LogProcessor{
+    DebugLogProcessor(LogProcessor nextlogProcessor) {
+        super(nextlogProcessor);
+    }
+    public void log(int logLevel,String message){
+        if(logLevel == DEBUG) {
+            System.out.println("DEBUG: " + message);
+        } else{
+            super.log(logLevel, message);
+        }
+    }
+}
